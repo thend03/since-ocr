27ab3ae4f69dbe6fa0aff0add9a5b628eb6b114e
since <fengchuang033@zto.com>
2023-04-11 22:43:21 +0800

    feat(deploy): add deploy
    添加部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull origin main
-mvn clean package
+mvn clean package -Dmaven.test.skip=true
 ps -ef|grep since-ocr|grep -v grep|awk '{print $2}'|xargs -r kill
 nohup java -jar target/since-ocr-0.0.1-SNAPSHOT.jar >/dev/null &
 
